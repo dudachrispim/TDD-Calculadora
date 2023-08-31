5bf6ac8d5c76c1198696dad7a5e2c6ce170a0c4e
BeatrizOs1 <76188108+BeatrizOs1@users.noreply.github.com>
2023-08-31 19:19:04 -0300

    Add files via upload

--- TDD-Java-Calculadora/src/main/java/Calculadora.java
+++ TDD-Java-Calculadora/src/main/java/Calculadora.java
@@ -1,12 +1,5 @@
-package main.java;
-
 public class Calculadora {
-	public int multiplicacao(int a, int b) {
-        return a * b;
+    public int sub(int a, int b) {
+        return a - b;
     }
-
-public  int divisao (int d, int e){
-        return d/e;
-    }
-	
 }
